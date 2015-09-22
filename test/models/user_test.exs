cd81c6b69cc2245f56b63034178e5a349796dfca
Gabriel Jaldon <gjaldon85@gmail.com>
2015-09-22 11:55:15 +0800

    Spec out callbacks in test

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -2,17 +2,50 @@ defmodule FileUpload.UserTest do
   use FileUpload.ModelCase
 
   alias FileUpload.User
+  alias FileUpload.Repo
 
-  @valid_attrs %{avatar: "some content", email: "some content"}
-  @invalid_attrs %{}
-
-  test "changeset with valid attributes" do
-    changeset = User.changeset(%User{}, @valid_attrs)
-    assert changeset.valid?
+  def fake_upload do
+    %Plug.Upload{filename: "test", path: Path.expand("./test/fixtures/test.jpg")}
   end
 
-  test "changeset with invalid attributes" do
-    changeset = User.changeset(%User{}, @invalid_attrs)
-    refute changeset.valid?
+  setup do
+    upload_path = Application.get_env(:file_upload, :upload_path)
+    unless File.exists?(upload_path) do
+      File.mkdir_p!(upload_path)
+    end
+
+    on_exit fn ->
+      File.rm_rf Path.expand "../priv/", __DIR__
+    end
+    :ok
+  end
+
+  test "uploads file on insert" do
+    params = %{avatar_upload: fake_upload()}
+    %{avatar: avatar} = Repo.insert! User.changeset(%User{}, params)
+
+    assert avatar
+    assert avatar.filename == params.avatar_upload.filename
+    assert File.exists? Path.expand("../.." <> avatar.filepath, __DIR__)
+  end
+
+  test "uploads file on update" do
+    user = Repo.insert! %User{}
+    params = %{avatar_upload: fake_upload()}
+    %{avatar: avatar} = Repo.update! User.changeset(user, params)
+
+    assert avatar
+    assert avatar.filename == params.avatar_upload.filename
+    assert File.exists? Path.expand(".." <> user.avatar.filepath, __DIR__)
+  end
+
+  test "removes old file on update" do
+    params = %{avatar_upload: fake_upload()}
+    user = User.changeset(%User{}, params)
+           |> Repo.insert!
+           |> Map.put(:avatar_upload, nil)
+    Repo.update!(User.changeset(user, params))
+
+    refute File.exists? Path.expand(".." <> user.avatar.filepath, __DIR__)
   end
 end
